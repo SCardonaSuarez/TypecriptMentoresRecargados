7c634f0618beefb38a9286b4ae5780628b7f15f8
Santo <santicardonas777@gmail.com>
2021-09-11 00:37:58 -0500

    Se refactorizo la terminal de mentores con Interfaces, Clases abstractas y Genéricos

---
+++ src/Persona.ts
@@ -0,0 +1,33 @@
+interface people {
+  nombre: string;
+  documento: string;
+  telefono: string;
+  email: string;
+}
+
+export abstract class Persona implements people {
+  nombre;
+  documento;
+  telefono;
+  email;
+
+  constructor(
+    nombre: string,
+    documento: string,
+    telefono: string,
+    email: string
+  ) {
+    this.nombre = nombre;
+    this.documento = documento;
+    this.telefono = telefono;
+    this.email = email;
+  }
+
+  getNombre(): string {
+    return this.nombre;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
+}
